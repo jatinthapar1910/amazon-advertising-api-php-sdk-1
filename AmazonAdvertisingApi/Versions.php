d6967e862b653c0639d5184b43e6fbf3dfb0721c
xyrintech <service@xyrintech.com>
2019-03-06 16:54:13 +0530

    Update Versions.php

--- AmazonAdvertisingApi/Versions.php
+++ AmazonAdvertisingApi/Versions.php
@@ -7,4 +7,9 @@ class Versions
         "apiVersion"         => "v1",
         "applicationVersion" => "1.2"
     );
+    
+    public $versionStringsV2 = array(
+        "apiVersion"         => "v2",
+        "applicationVersion" => "1.2"
+    );
 }
